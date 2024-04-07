1ad0b50d536eaa013c6c62720be5f6469de0d372
Samuel Almeida <samuel@bravion.dev>
2024-04-07 12:18:14 -0300

    Add readln function

--- hello_world.asm
+++ hello_world.asm
@@ -1,15 +1,17 @@
 .global _start             // Provide program starting address to linker
 .p2align 3
 
+
+.text
 _strlen:
         mov X0, #0
-loop:
+_strlen_loop:
         ldrb W2, [X1, X0]
         cmp W2, #0
-        beq done
+        beq _strlen_done
         add X0, X0, #1
-        b loop
-done:
+        b _strlen_loop
+_strlen_done:
         ret 
 
 
@@ -37,8 +39,31 @@ _println:
 
         adr X1, newline
         mov X0, #1
+        mov X2, #1
         mov X16, #4
         svc #0x80
+
+        ldp X29, X30, [SP], #16
+        ret
+
+_readln:
+        stp X29, X30, [SP, #-16]!
+        mov X29, SP
+
+        // Read user input into inputBuffer
+        mov X0, #0
+        adrp X1, inputBuffer@page
+        add X1, X1, inputBuffer@pageoff
+        mov X2, #63
+        mov X16, #3
+        svc #0x80
+
+        // Replace newline with null terminator
+        adrp X1, inputBuffer@page
+        add X1, X1, inputBuffer@pageoff
+        subs X2, X0, #1               // Subtract 1 from the number of bytes read to get the last byte's index
+        add X3, X1, X2                // X3 = address of the last byte read
+        strb wzr, [X3]               // Replace newline with null terminator
 
         ldp X29, X30, [SP], #16
         ret
@@ -56,6 +81,17 @@ _start:
         adr X1, cake
         bl _print
 
+        adr X1, question
+        bl _print
+        bl _readln
+
+        adr X1, helloyou
+        bl _print
+
+        adrp X1, inputBuffer@page
+        add X1, X1, inputBuffer@pageoff
+        bl _println
+
         mov     X0, #0
         mov     X16, #1
         svc     #0x80
@@ -64,3 +100,8 @@ helloworld:     .asciz  "Hello World!"
 othermessage:   .asciz  "This is Apple Silicon ASM!\n"
 cake:           .asciz  "The cake is a lie!\n"
 newline:        .asciz  "\n"
+question:       .asciz  "What is your name?: "
+helloyou:       .asciz  "Hello, "
+
+.data
+inputBuffer:    .space  64
